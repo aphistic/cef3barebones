6be2c708fd0b87b7e822cb2975b5746b6ab4bc5e
Erik Davidson <erik@erikd.org>
2013-01-18 21:35:07 -0600

    Add a warning comment in regard to the IMPLEMENT_REFCOUNTING macro

--- include/bareboneshandler.h
+++ include/bareboneshandler.h
@@ -9,6 +9,12 @@ public:
   BareBonesHandler() { }
   ~BareBonesHandler() { }
 
+  // Note that any of the IMPLEMENT_WHATEVER
+  // macros that come with CEF can (and do) set
+  // access modifiers, so you'll want them after
+  // everything else in your class or you may be
+  // in for a surprise when the access of a member
+  // isn't what you expect it to be!
   IMPLEMENT_REFCOUNTING(BareBonesHandler);
 };
 
