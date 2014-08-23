e875e5ed345f1ad4526b28f9bad3645d3867702c
Erik Davidson <erik@erikd.org>
2014-08-23 03:36:39 -0500

    Add additional command line switch so you don't need to specify the library path in an env var to run the barebones binary

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ if [ ! -d "build" ]; then
   mkdir build
 fi
 
-g++ -Iinclude -Icef3 -o build/barebones `pkg-config --cflags gtk+-2.0` barebones.cc -Lcef3/bin/linux/$1/cef -lcef -lcef_dll_wrapper `pkg-config --libs gtk+-2.0`
+g++ -Iinclude -Icef3 -o build/barebones `pkg-config --cflags gtk+-2.0` barebones.cc -Lcef3/bin/linux/$1/cef -lcef -lcef_dll_wrapper -Wl,-rpath,. `pkg-config --libs gtk+-2.0`
 cp cef3/bin/linux/$1/cef/*.so build/
 if [ ! -d "build/locales" ]; then
   mkdir build/locales/
